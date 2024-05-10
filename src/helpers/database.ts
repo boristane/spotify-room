5f170b1b7042b7b3a0a42a28a05702f73c2af43c
Boris Tane <boris.tane@gmail.com>
2024-05-10 16:52:43 +0100

    add this

--- src/helpers/database.ts
+++ src/helpers/database.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export async function connectToDb() {
-  const uri = `mongodb+srv://rooom:${process.env.MONGO_ATLAS_PASSWORD}@eclecticdata.zt9sk.mongodb.net/?retryWrites=true&w=majority&appName=eclecticData`;
+  const uri = `mongodb+srv://rooom:${process.env.MONGO_ATLAS_PASSWORD}@eclecticdata.zt9sk.mongodb.net/room/?retryWrites=true&w=majority&appName=eclecticData`;
   mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 }
